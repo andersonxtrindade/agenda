291cdcc089254125c2430f098be448d24ebe1384
Anderson Souza <axtrindade2012@yahoo.com>
2020-08-03 22:40:21 -0300

    primeiro commit

---
+++ contatos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)#Blank torna o campo opcional
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/%d')
+
